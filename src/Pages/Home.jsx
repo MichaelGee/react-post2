b0438c8053180cc5b3c3547342627f701432e1b1
michael.gates <michaelgatesjr@gmail.com>
2021-06-20 21:19:57 +0100

    fix: made the inital useState of data an empty array

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,53 +1,37 @@
 import React from "react";
 import axios from "axios";
-import '../styles/main.css';
+import "../styles/main.css";
 // import useFetch from '../hooks/useFetch';
 import { useState, useEffect } from "react";
 import PostList from "./PostsList";
 
-
-
-
-
 const Home = () => {
+  const [data, setData] = useState([]);
 
- const [ data, setData] = useState({})
-
-
- const getData = async() => {
+  const getData = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/posts')
-      setData(response.data)
+      const response = await axios.get("http://localhost:8000/posts");
+      setData(response.data);
     } catch (error) {
-      return error
-    } 
- }
- 
- useEffect(() => {
-  getData()
- }, [])
+      return error;
+    }
+  };
 
+  useEffect(() => {
+    getData();
+  }, []);
 
+  return (
+    <div className="home">
+      {/* { isPending && <div className='loading'> Please wait, your page is loading... </div>} */}
 
-  
-
- return(
-   <div className="home">
-
-     {/* { isPending && <div className='loading'> Please wait, your page is loading... </div>} */}
-
-        {/* since the value of post is null at the very start, we  add a conditional rendering 
+      {/* since the value of post is null at the very start, we  add a conditional rendering 
           to state that since userpost is null it wont evaluate the code at && instead it'll stop running
           but if post( what you are mapping) has a value then Card will render.  */}
-        
-        {data && <PostList data={data} />}
-    
-          
-  
-   </div>
- );
+
+      {data && <PostList data={data} />}
+    </div>
+  );
 };
 
-
 export default Home;
-
--- src/Pages/PostsList.jsx
+++ src/Pages/PostsList.jsx
@@ -1,25 +1,11 @@
 import Card from "../components/card";
 
-
-const PostList = ({ data}) => {
-
-    // const posts = props.posts; 
-    console.log(data)   
-
-    return(
-        <div className="post-list">
-            
-      {/* this creates a new array containing cards and it is used to render all our cards.    */}
-     
-     {/* { data && data?.map((item) => (
-         <Card  item={item}/>
-          
-     ) )} */}
-
-     
-
-        </div>
-    )
-}
+const PostList = ({ data }) => {
+  return (
+    <div className="post-list">
+      {data && data?.map((item) => <Card item={item} />)}
+    </div>
+  );
+};
 
 export default PostList;
